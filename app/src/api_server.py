ab921044d1929cc96f2637de34be108d151d255b
apalma <apalma@freepik.com>
2025-05-13 09:09:13 +0200

    refactor: remove unused code, no nginx deployment needed. Remove lock charts

--- app/src/api_server.py
+++ app/src/api_server.py
@@ -2,6 +2,7 @@
 import logging
 import uvicorn
 from src.api.celery_api import app
+from prometheus_fastapi_instrumentator import Instrumentator
 
 # Configure logging
 logging.basicConfig(
@@ -10,6 +11,9 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
+# Add Prometheus metrics instrumentation
+Instrumentator().instrument(app).expose(app)
+
 if __name__ == "__main__":
     logger.info("Starting FastAPI server...")
-    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="info") 
+    uvicorn.run("src.api.celery_api:app", host="0.0.0.0", port=8000, log_level="info", workers=4) 
